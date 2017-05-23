6c67314c185ed7b55732b717e69e7da662c25753
Anton Sherstiuk <Anton Sherstiuk>
2017-05-23 20:46:09 +0300

    Homework 1

---
+++ Assets/HeroFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public HeroRabbit rabbit;
+
+	void Update () {
+		Transform rabbit_transform = rabbit.transform;
+		Transform camera_transform = this.transform;
+		
+		Vector3 rabbit_position = rabbit_transform.position;
+		Vector3 camera_position = camera_transform.position;
+
+		camera_position.x = rabbit_position.x;
+		camera_position.y = rabbit_position.y;
+		
+		camera_transform.position = camera_position;
+	}
+}
